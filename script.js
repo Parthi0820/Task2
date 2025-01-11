c88c059542aa18a84dbcbdb4426b0d4fe2fc499b
Parthi0820 <147618088+Parthi0820@users.noreply.github.com>
2025-01-11 08:49:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var swiper = new Swiper(".mySwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      type: "fraction",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    autoplay: {
+      delay: 3000, 
+      disableOnInteraction: false, 
+    },
+  });
+  
